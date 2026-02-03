b5e165ad8261496891d709efd1b337939acf2eff
Michael Strickling <m.strickling@proton.me>
2026-02-03 11:04:05 +0100

    Second Commit; Changes in Design [Header & Footer fixed in Position, grid in main-class geaendert]

--- index.php
+++ index.php
@@ -6,35 +6,34 @@ include __DIR__ . '/includes/sidebar.php';
 
 
 <main class="grid">
+  <a href="<?php echo BASE_URL; ?>/sub_directories/pos.php" class="module-link module-pos">
+    <div class="module">
+      <h2>Point of Sale</h2>
+      <p>Weiterleitung zu Point of Sale</p>
+    </div>
+  </a>
 
-    <a href="<?php echo BASE_URL; ?>/sub_directories/pos.php" class="module-link">
-        <div class="module">
-            <h2>Point of Sale</h2>
-            <p>Weiterleitung zu Point of Sale</p>
-        </div>
-    </a>
+  <a href="<?php echo BASE_URL; ?>/sub_directories/lagerverwaltung.php" class="module-link module-lager">
+    <div class="module">
+      <h2>Lagerverwaltung</h2>
+      <p>Weiterleitung zur Lagerverwaltung</p>
+    </div>
+  </a>
 
-    <a href="<?php echo BASE_URL; ?>/sub_directories/lagerverwaltung.php" class="module-link">
-        <div class="module">
-            <h2>Lagerverwaltung</h2>
-            <p>Weiterleitung zur Lagerverwaltung</p>
-        </div>
-    </a>
+  <a href="<?php echo BASE_URL; ?>/sub_directories/admin.php" class="module-link module-admin">
+    <div class="module">
+      <h2>Administration</h2>
+      <p>Weiterleitung zum Admin-Panel</p>
+    </div>
+  </a>
 
-    <a href="<?php echo BASE_URL; ?>/sub_directories/admin.php" class="module-link">
-        <div class="module">
-            <h2>Administration</h2>
-            <p>Weiterleitung zum Admin-Panel</p>
-        </div>
-    </a>
-
-    <a href="<?php echo BASE_URL; ?>/sub_directories/einstellungen.php" class="module-link">
-        <div class="module">
-            <h2>Einstellungen</h2>
-            <p>Weiterleitung zu den Einstellungen</p>
-        </div>
-    </a>
-
+  <a href="<?php echo BASE_URL; ?>/sub_directories/einstellungen.php" class="module-link module-settings">
+    <div class="module">
+      <h2>Einstellungen</h2>
+      <p>Weiterleitung zu den Einstellungen</p>
+    </div>
+  </a>
 </main>
+
 
 <?php include __DIR__ . '/includes/footer.php'; ?>
